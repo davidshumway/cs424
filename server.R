33329160800644905a8a9bcf85c3ec26e9276da7
their <davidshumway@gmail.com>
2021-02-13 19:38:28 -0600

    Finish 40% section

--- server.R
+++ server.R
@@ -34,15 +34,6 @@ b1 <- subset(b1, ENERGY.SOURCE != 'Total') # remove 'Total'
 b1 <- subset(b1, TYPE.OF.PRODUCER == 'Total Electric Power Industry')
 # line chart percent
 # table 2 pct
-#b2 <- subset(data, STATE == 'US-TOTAL')
-#b2 <- subset(b2, ENERGY.SOURCE == 'Total')
-#~ fnpct <- function(x) {
-#~   100 * x / sum(x)
-#~ }
-#~ print(fnpct)
-#~ b2 <- do.call('rbind', as.list(
-#~   by(b1, b1['YEAR'], transform, Pct = env().fnpct(GEN))
-#~ ))
 library(data.table)
 dt <- data.table(b1)
 setkey(dt, "YEAR") 
@@ -51,10 +42,50 @@ b2 <- dt[X,
   list(ENERGY.SOURCE, GEN, YEAR, PCT=round(100*GEN/SUM, digits = 1))
 ]
 
-
+# filters b1 and b2 data based on checkboxes
+doFilters <- function(input, data) {
+  c = data
+  if (!input$filter1) {
+    if (!input$filter2) c <- subset(c, ENERGY.SOURCE != 'Coal')
+    if (!input$filter3) c <- subset(c, ENERGY.SOURCE != 'Geothermal')
+    if (!input$filter4) c <- subset(c, ENERGY.SOURCE != 'Hydro')
+    if (!input$filter5) c <- subset(c, ENERGY.SOURCE != 'Natural Gas')
+    if (!input$filter6) c <- subset(c, ENERGY.SOURCE != 'Nuclear')
+    if (!input$filter7) c <- subset(c, ENERGY.SOURCE != 'Petroleum')
+    if (!input$filter8) c <- subset(c, ENERGY.SOURCE != 'Solar')
+    if (!input$filter9) c <- subset(c, ENERGY.SOURCE != 'Wind')
+    if (!input$filter10)c <- subset(c, ENERGY.SOURCE != 'Wood')
+  }
+  c
+}
 
 shinyServer(function(input, output) {
+#~   print(input)
+#~          checkboxInput('filter1', 'All', TRUE),
+#~          checkboxInput('filter2', 'Coal', FALSE),
+#~          checkboxInput('filter3', 'Geothermal', FALSE),
+#~          checkboxInput('filter4', 'Hyrdo', FALSE),
+#~          checkboxInput('filter5', 'Nat. Gas', FALSE),
+#~          checkboxInput('filter6', 'Nuclear', FALSE),
+#~          checkboxInput('filter7', 'Petroleum', FALSE),
+#~          checkboxInput('filter8', 'Solar', FALSE),
+#~          checkboxInput('filter9', 'Wind', FALSE),
+#~          checkboxInput('filter10','Wood', FALSE)
+#~   b1 <- reactive({
+#~     if (!input$filter1) {
+#~       b1
+#~     } else {
+#~       b1
+#~     }
+#~     subset(allData, year(allData$newDate) == input$Year)
+#~   })
+#~   a <- reactive(input$filter1)
+#~   print(a)
+#~   b1 <- reactive({
+#~     subset(b1, year(allData$newDate) == input$Year)
+#~   })
   
+#~   
 #~     newNoons <- newNoonsReactive()
 #~     temperatures <- as.data.frame(table(newNoons[,input$Room]))
 #~     temperatures$Var1 <- as.numeric(as.character(temperatures$Var1))
@@ -62,8 +93,8 @@ shinyServer(function(input, output) {
   # stacked bar chart showing amount of each energy source per year
   # from 1990 - 2019 (assumption: for entire US?)
   output$bar1 <- renderPlot({
-    ggplot(b1,
-      aes(x = YEAR, y = GEN)) + 
+    a = doFilters(input, b1)
+    ggplot(a, aes(x = YEAR, y = GEN)) + 
       geom_col(aes(fill = ENERGY.SOURCE), width = 0.7) +
       labs(x = 'Year', y = 'Count')
   })
@@ -73,7 +104,8 @@ shinyServer(function(input, output) {
 #~   n1 <- as.numeric(as.character(b1$GEN))
 #~   n2 <- as.numeric(as.character(b2$GEN))
   output$bar2 <- renderPlot({
-    ggplot(b1, aes(x = YEAR, y = GEN, fill = ENERGY.SOURCE)) + 
+    a = doFilters(input, b1)
+    ggplot(a, aes(x = YEAR, y = GEN, fill = ENERGY.SOURCE)) + 
       geom_bar(position = 'fill', stat = 'identity') +
       labs(x = 'Year', y = 'Count')
   })
@@ -81,7 +113,8 @@ shinyServer(function(input, output) {
   # line chart showing the amount of each energy source per year from
   # 1990 - 2019
   output$line1 <- renderPlot({
-    ggplot(b1, aes(x = YEAR, y = GEN, fill = ENERGY.SOURCE, color = ENERGY.SOURCE)) +
+    a = doFilters(input, b1)
+    ggplot(a, aes(x = YEAR, y = GEN, fill = ENERGY.SOURCE, color = ENERGY.SOURCE)) +
       geom_line(stat = 'identity') +
       labs(x = 'Year', y = 'Energy produced')
   })
@@ -89,6 +122,7 @@ shinyServer(function(input, output) {
   # line chart showing the percent of the total production for each
   # energy source per year from 1990 - 2019
   output$line2 <- renderPlot({
+    a = doFilters(input, b2)
     ggplot(b2, aes(x = YEAR, y = PCT, fill = ENERGY.SOURCE, color = ENERGY.SOURCE)) +
       geom_line(stat = 'identity') +
       labs(x = 'Year', y = 'Percentage of total energy produced')
@@ -99,7 +133,9 @@ shinyServer(function(input, output) {
   #newNoonsReactive <- reactive({subset(allData, year(allData$newDate) == input$Year & Hour == 12)})
   output$tab1 <- DT::renderDataTable(
     DT::datatable(
-      b1[, c('YEAR','GEN','ENERGY.SOURCE')], # arrange(.., ..)
+      doFilters(input, b1)[, c('YEAR','GEN','ENERGY.SOURCE')],
+      # similar option is arrange(.., ..)
+#~       b1[, c('YEAR','GEN','ENERGY.SOURCE')], 
 #~       {
 #~         newOne <- reactive({b1})
 #~           b1
@@ -120,7 +156,7 @@ shinyServer(function(input, output) {
   # each energy source per year from 1990 - 2019
   output$tab2 <- DT::renderDataTable(
     DT::datatable(
-      b2[, c('YEAR','GEN','ENERGY.SOURCE', 'PCT')], # arrange(.., ..)
+      doFilters(input, b2)[, c('YEAR','GEN','ENERGY.SOURCE', 'PCT')],
 #~       {
 #~         newOne <- reactive({b1})
 #~           b1
--- ui.R
+++ ui.R
@@ -5,23 +5,75 @@ library(shiny)
 library(shinydashboard)
 library(ggplot2)
 
-options(shiny.fullstacktrace = FALSE)
+options(shiny.fullstacktrace = TRUE)
 
-
+#~               var x = $('#filter1').parent();#.get(0);
+#~               console.log(x);
+#~               var cb = $('input');
+#~               console.log(cb);
+#~               cb.prop('checked', true); //!checkBoxes.prop('checked')
+#~               x.prop('checked', true);
 dashboardPage(
-  dashboardHeader(title = "CS 424 Spring 2020 Example Dashboard"),
+  dashboardHeader(title = 'CS 424 Spring 2020: Project 1'),
   dashboardSidebar(
+     tags$head(    # checkbox JS, adapted from
+      tags$script( # https://stackoverflow.com/questions/32847743/
+        HTML(      # switching-between-menusubitems-in-shinydashboard
+          "
+#~           $(document).ready(function() {
+#~             $('#filter1').on('click', function() {
+#~               console.log('x');
+#~               console.log($(this).parent());
+#~               console.log($(this).parent('input'));
+#~               var x = $(this).parent().parent().get(0).getElementsByTagName('input');
+#~               console.log(x);
+#~               for (var i=0; i<x.length; i++) {
+#~                 x[i].checked = true; //$(this).attr('checked');
+#~               }
+#~             })
+#~           })
+          "
+        )
+      )
+    ),
+    
 #~       disable = FALSE,
 #~       collapsed = FALSE,
     sidebarMenu(
       menuItem('Stacked', tabName = 'stacked', icon = NULL),
       menuItem('Line',    tabName = 'line', icon = NULL),
-      menuItem('Table',   tabName = 'table', icon = NULL)
+      menuItem('Table',   tabName = 'table', icon = NULL),
       
       # menu: select energy sources <all, 1,2,3,4,5>
+      menuItem('Sources', icon = NULL,
+#~      collapsible =  ??????
+        checkboxInput('filter1', 'All', TRUE),
+        checkboxInput('filter2', 'Coal', FALSE),
+        checkboxInput('filter3', 'Geothermal', FALSE),
+        checkboxInput('filter4', 'Hyrdo', FALSE),
+        checkboxInput('filter5', 'Nat. Gas', FALSE),
+        checkboxInput('filter6', 'Nuclear', FALSE),
+        checkboxInput('filter7', 'Petroleum', FALSE),
+        checkboxInput('filter8', 'Solar', FALSE),
+        checkboxInput('filter9', 'Wind', FALSE),
+        checkboxInput('filter10','Wood', FALSE)
+      )
+              
+#~             menuSubItem(
+#~         menuSubItem('All', icon = NULL,
+#~           checkboxInput('somevalue', 'All', FALSE)
+#~         ),
+#~         menuSubItem("Sub-item 2")
+#~       box(
+#~         title = 'Energy source(s)', solidHeader = TRUE,
+#~ #         'Box content here', br(), 'More box content',
+#~ #         sliderInput('slider', 'Slider input:', 1, 100, 50),
+#~         checkboxInput('somevalue', 'All', FALSE),
+#~         checkboxInput('somevalue', 'Coal', FALSE)
+#~       )
       
-#~         selectInput("Year", "Select the year to visualize"),
-#~         selectInput("Room", "Select the room to visualize", listNamesGood, selected = "Meeting Room")
+#~         selectInput('Year', 'Select the year to visualize'),
+#~         selectInput('Room', 'Select the room to visualize', listNamesGood, selected = 'Meeting Room')
     )
   ),
   dashboardBody(
@@ -32,7 +84,7 @@ dashboardPage(
           column(6,
             fluidRow(
               box(title = 'Annual energy by source',
-                solidHeader = TRUE, status = "primary", width = 12,
+                solidHeader = TRUE, status = 'primary', width = 12,
                 plotOutput('bar1', height = 300)
               )
             )
@@ -40,11 +92,11 @@ dashboardPage(
           column(6,
             fluidRow(
               box(title = 'Annual energy by source %',
-                solidHeader = TRUE, status = "primary", width = 12,
+                solidHeader = TRUE, status = 'primary', width = 12,
                 plotOutput('bar2', height = 300)
               )
             )
-          ),
+          )
         )
       ),
       
@@ -54,7 +106,7 @@ dashboardPage(
           column(6,
             fluidRow(
               box(title = 'Annual energy by source',
-                solidHeader = TRUE, status = "primary", width = 12,
+                solidHeader = TRUE, status = 'primary', width = 12,
                 plotOutput('line1', height = 300)
               )
             )
@@ -62,11 +114,11 @@ dashboardPage(
           column(6,
             fluidRow(
               box(title = 'Annual energy by source %',
-                solidHeader = TRUE, status = "primary", width = 12,
+                solidHeader = TRUE, status = 'primary', width = 12,
                 plotOutput('line2', height = 300)
               )
             )
-          ),
+          )
         )
       ),
       
@@ -76,7 +128,7 @@ dashboardPage(
           column(6,
             fluidRow(
               box(title = 'Annual energy by source',
-                solidHeader = TRUE, status = "primary", width = 12,
+                solidHeader = TRUE, status = 'primary', width = 12,
                 dataTableOutput('tab1', height = 300)
               )
             )
@@ -84,11 +136,11 @@ dashboardPage(
           column(6,
             fluidRow(
               box(title = 'Annual energy by source %',
-                solidHeader = TRUE, status = "primary", width = 12,
+                solidHeader = TRUE, status = 'primary', width = 12,
                 dataTableOutput('tab2', height = 300)
               )
             )
-          ),
+          )
         )
       )
     )
@@ -100,13 +152,13 @@ dashboardPage(
 #~ shinyUI(fluidPage(
 
 #~   # Application title
-#~   titlePanel("Old Faithful Geyser Data"),
+#~   titlePanel('Old Faithful Geyser Data'),
 
 #~   # Sidebar with a slider input for number of bins
 #~   sidebarLayout(
 #~     sidebarPanel(
-#~       sliderInput("bins",
-#~                   "Number of bins:",
+#~       sliderInput('bins',
+#~                   'Number of bins:',
 #~                   min = 1,
 #~                   max = 50,
 #~                   value = 30)
@@ -114,7 +166,7 @@ dashboardPage(
 
 #~     # Show a plot of the generated distribution
 #~     mainPanel(
-#~       plotOutput("distPlot")
+#~       plotOutput('distPlot')
 #~     )
 #~   )
 #~ ))
